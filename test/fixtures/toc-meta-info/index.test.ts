24006919fbbf5e2f555f57b7b5efcbda361a52b9
dxd <dxd_sjtu@outlook.com>
2019-11-16 08:32:33 +0800

    feat: but kiki loves to dance (#2)

--- test/fixtures/toc-meta-info/index.test.ts
+++ test/fixtures/toc-meta-info/index.test.ts
@@ -12,24 +12,24 @@ describe('test/fixtures/toc-meta-info/index.test.ts', () => {
     const input = fs.readFileSync(path.join(__dirname, 'index.md'), 'utf8');
     showdown.makeHtml(input);
     expect(toc).toEqual([
-      { anchor: 'usereducer和redux的区别：', level: '2', text: 'useReducer和redux的区别：' },
-      { anchor: '思路：', level: '2', text: '思路：' },
+      { anchor: 'usereducer和redux的区别：', level: 2, text: 'useReducer和redux的区别：' },
+      { anchor: '思路：', level: 2, text: '思路：' },
       {
         anchor: '第一步，通过usereducer在根组件上创建globaldispatch和globalstate：',
-        level: '3',
+        level: 3,
         text: '第一步，通过useReducer在根组件上创建globalDispatch和globalState：',
       },
       {
         anchor: '第二步：-创建context并在根组件上传入globalstate和globaldispatch',
-        level: '3',
+        level: 3,
         text: '第二步： 创建context并在根组件上传入globalState和globalDispatch',
       },
       {
         anchor: '在子组件中使用globalstate和globaldispatch：',
-        level: '3',
+        level: 3,
         text: '在子组件中使用globalState和globalDispatch：',
       },
-      { anchor: '总结和建议：', level: '3', text: '总结和建议：' },
+      { anchor: '总结和建议：', level: 3, text: '总结和建议：' },
     ]);
   });
 });
